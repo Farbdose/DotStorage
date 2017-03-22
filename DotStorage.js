a5111272e71a772d47ba801dd409a4acc0afd19f
Farbdose <fabian@nitsche-gera.de>
2017-03-22 15:51:35 +0100

    added deep property tracking and a little cache

--- DotStorage.js
+++ DotStorage.js
@@ -1,24 +1,72 @@
 (function () {
+    function traverse(o,func) {
+        for (var i in o) {
+            if(o.hasOwnProperty(i)){
+                if (o[i] !== null && typeof(o[i]) == "object") {
+                    //going one step down in the object tree!!
+                    traverse(o[i], func);
+                }
+                func.apply(this, [o, i, o[i]]);
+            }
+        }
+    }
+
+    var DeepProxy = function (obj, callback) {
+        if(obj !== Object(obj) || obj.isDeepProxy) return obj;
+
+        console.debug("DeepInit of: ",obj);
+        traverse(obj, function (o, key, val) {
+            o[key] = DeepProxy(val, callback)
+        });
+
+        var handlers = {
+            set: function (innerObj, innerKey, innerValue) {
+                //console.debug("Called inner set with: ", innerKey, innerValue);
+                if(innerValue === Object(innerValue) && !innerValue.isDeepProxy)
+                    innerValue = DeepProxy(innerValue, callback);
+
+                innerObj[innerKey] = innerValue;
+                callback();
+                return true
+            }
+        };
+
+        obj.isDeepProxy = true;
+
+        return new Proxy(obj, handlers);
+    };
+
+    var dotCache = {};
+
     var handlers = {
         set: function (storage, key, value) {
             console.debug("Called set with: ", key, value);
-            storage.setItem(key, JSON.stringify(value));
+            var clone = JSON.parse(JSON.stringify(value));
+            delete clone.isDeepProxy;
+            storage.setItem(key, JSON.stringify(clone));
+            dotCache[key] = DeepProxy(value, function () {
+                handlers.set(storage, key, value);
+            });
             return true
         },
 
         get: function(storage, key){
             console.debug("Called get with: ", key);
 
-            obj = JSON.parse(storage.getItem(key));
-            innerHandler = {
-                set: function(innerObj, innerKey, innerValue){
-                    console.debug("Called inner set with: ", innerKey, innerValue);
-                    innerObj[innerKey] = innerValue;
-                    handlers.set(storage, key, innerObj);
-                    return true
+            if(dotCache.hasOwnProperty(key)){
+                console.debug("Found in cache...");
+                return dotCache[key]
+            }else {
+                console.debug("Loading from local Storage...");
+                obj = JSON.parse(storage.getItem(key));
+                obj.isDeepProxy = false;
+                if(obj != null) {
+                    dotCache[key] = DeepProxy(obj, function () {
+                        handlers.set(storage, key, obj);
+                    });
                 }
-            };
-            return new Proxy(obj, innerHandler);
+                return dotCache[key];
+            }
         },
 
         has: function(storage, key) {
@@ -30,31 +78,10 @@
             console.debug("Called defineProperty with: ", key, value);
             storage.setItem(key, JSON.stringify(value))
         }
-    }
+    };
 
     window.dotStorage = new Proxy(window.localStorage, handlers);
 }());
 
-
-dotStorage.stringTest = {
-    "Hello": "World"
-};
-console.log(dotStorage.test.Hello);
-
-dotStorage.arrayTest = [1,2,3];
-dotStorage.arrayTest.forEach(function(e){console.log(e)});
-
-dotStorage.complexArrayTest = [
-    {"Hello": "World", "index": 0},
-    {"Hello": "World2", "index": 1},
-    {"Hello": "World3", "index": 2}
-];
-dotStorage.complexArrayTest.forEach(function(e){console.log(e.index+": "+e.Hello)});
-
-var myObj = {"New": "object"};
-dotStorage.refTest = myObj;
-myObj = dotStorage.refTest;
-myObj.New = "other object";
-console.log(dotStorage.refTest.New);
 
 
