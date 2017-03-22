21140bcf68eb6a424ad30ac6f6acd99315c0ca46
Farbdose <fabian@nitsche-gera.de>
2017-03-22 16:22:44 +0100

    added comments and removed debug messages

--- DotStorage.js
+++ DotStorage.js
@@ -1,7 +1,9 @@
 (function () {
-    function traverse(o,func) {
+    /* traverse object tree from given object and call func on all properties
+     found here: http://stackoverflow.com/a/722732/2422125 */
+    function traverse(o, func) {
         for (var i in o) {
-            if(o.hasOwnProperty(i)){
+            if (o.hasOwnProperty(i)) {
                 if (o[i] !== null && typeof(o[i]) == "object") {
                     //going one step down in the object tree!!
                     traverse(o[i], func);
@@ -11,75 +13,83 @@
         }
     }
 
+    /* wrap a given object in a proxy and call callback on any property change
+     also do the same for all properties of the given object that are objects themself
+     finally when a change is detected and the new value isn't a DeepProxy, wrap it in one */
     var DeepProxy = function (obj, callback) {
-        if(obj !== Object(obj) || obj.isDeepProxy) return obj;
+        // return if obj isn't an object or already a DeepProxy
+        if (obj !== Object(obj) || obj.isDeepProxy) return obj;
 
-        console.debug("DeepInit of: ",obj);
+        // wrap all properties of obj in DeepProxies
         traverse(obj, function (o, key, val) {
             o[key] = DeepProxy(val, callback)
         });
 
+        // set handler to intercept property changes
         var handlers = {
             set: function (innerObj, innerKey, innerValue) {
-                //console.debug("Called inner set with: ", innerKey, innerValue);
-                if(innerValue === Object(innerValue) && !innerValue.isDeepProxy)
-                    innerValue = DeepProxy(innerValue, callback);
-
-                innerObj[innerKey] = innerValue;
+                // wrap the new value in a DeepProxy
+                innerObj[innerKey] = DeepProxy(innerValue, callback);
                 callback();
                 return true
             }
         };
 
+        // mark this object as a DeepProxy
         obj.isDeepProxy = true;
 
         return new Proxy(obj, handlers);
     };
 
+    // cache results from localStorage so we don't have to wrap everything again in a DeepProxy on access
     var dotCache = {};
 
     var handlers = {
+        // intercept changes to the localStorage
         set: function (storage, key, value) {
-            console.debug("Called set with: ", key, value);
+            // clone the object to remove the DeepProxy marker befor saving it
             var clone = JSON.parse(JSON.stringify(value));
             delete clone.isDeepProxy;
+
+            // save the object in the localStorage
             storage.setItem(key, JSON.stringify(clone));
-            dotCache[key] = DeepProxy(value, function () {
-                handlers.set(storage, key, value);
+
+            // update the dotCache and use the clone to prevent interaction between multiple DeepProxy instances
+            dotCache[key] = DeepProxy(clone, function () {
+                handlers.set(storage, key, clone);
             });
             return true
         },
 
-        get: function(storage, key){
-            console.debug("Called get with: ", key);
-
-            if(dotCache.hasOwnProperty(key)){
-                console.debug("Found in cache...");
+        get: function (storage, key) {
+            if (dotCache.hasOwnProperty(key)) {
+                // if this key is in the dotCache just return the DeepProxy thats saves there
                 return dotCache[key]
-            }else {
-                console.debug("Loading from local Storage...");
+            } else {
+                // if not try to load it from the lcoalStorage
                 obj = JSON.parse(storage.getItem(key));
-                obj.isDeepProxy = false;
-                if(obj != null) {
+
+                // if there was an entry in the localStorage with the given key, add it to the dotCache
+                if (obj != null) {
+                    // flag the new object as not a DeepProxy (just to be sure)
+                    obj.isDeepProxy = false;
                     dotCache[key] = DeepProxy(obj, function () {
                         handlers.set(storage, key, obj);
                     });
                 }
+
+                // return the value of the dotCache with the given key, is undefined if the key wasn't in the localStorage
                 return dotCache[key];
             }
         },
 
-        has: function(storage, key) {
-            console.debug("Called has with: ", key);
-            return storage.key != null
-        },
-
-        defineProperty: function(storage, key, value) {
-            console.debug("Called defineProperty with: ", key, value);
-            storage.setItem(key, JSON.stringify(value))
+        // implement has function as localStorage contains non null value with this key
+        has: function (storage, key) {
+            return storage[key] != null
         }
     };
 
+    // public the created DotStorage
     window.dotStorage = new Proxy(window.localStorage, handlers);
 }());
 
--- DotStorageTest.js
+++ DotStorageTest.js
@@ -1,27 +1,43 @@
+// Note: actual ability to preserve saved values persistently is not tested
+
+// Test simple strings via dot
+dotStorage.Hello = "World";
+console.assert(dotStorage.Hello == "World");
+
+// Test simple strings via bracket
+dotStorage["Hello"] = "World";
+console.assert(dotStorage["Hello"] == "World");
+
+// Test simple objects
 dotStorage.stringTest = {
     "Hello": "World"
 };
 console.assert(dotStorage.stringTest.Hello == "World");
 
-input = [1,2,3];
+// Test array of primitives
+input = [1, 2, 3];
 dotStorage.arrayTest = input;
-dotStorage.arrayTest.forEach(function(i,e){console.assert(input[i]==e)});
+dotStorage.arrayTest.forEach(function (e, i) {
+    console.assert(input[i] == e)
+});
 
+// Test array of objects
 dotStorage.complexArrayTest = [
-    {"Hello": "World", "index": 0},
-    {"Hello": "World2", "index": 1},
-    {"Hello": "World3", "index": 2}
+    {"Hello": "World0", "index": 0},
+    {"Hello": "World1", "index": 1},
+    {"Hello": "World2", "index": 2}
 ];
-dotStorage.complexArrayTest.forEach(function(i,e){
-    console.assert(e.Hello == "World"+i);
+dotStorage.complexArrayTest.forEach(function (e, i) {
+    console.assert(e.Hello == "World" + i);
     console.assert(e.index == i)
 });
 
+// Test DeepProxy (changes to nested attributes are expected to be save in the localStorage too)
 var myObj = {"New": "object"};
 dotStorage.refTest = myObj;
 myObj = dotStorage.refTest;
 
-myObj.New = {"nested": { "otherObject": [0,1] }};
+myObj.New = {"nested": {"otherObject": [0, 1]}};
 console.assert(myObj.New.nested.otherObject.length == 2);
 
 myObj.New.nested = 2;
